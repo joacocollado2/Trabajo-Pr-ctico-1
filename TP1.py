3ea6000d48afe5c71700092e50e9831a8da63f06
Fragoz22 <frangomezfg22@gmail.com>
2024-05-02 21:18:55 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -5,8 +5,10 @@
 
 cp = input("Ingrese el código postal del lugar de destino: ")
 
-reales = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+# Establecer una bandera y poner en una tupla los numeros naturales
+
 ban = True
+reales = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
 if len(cp) > 9 or len(cp) < 4:
     ban = False
@@ -14,7 +16,8 @@ if len(cp) > 9 or len(cp) < 4:
         print("Otros Países")
 
 else:
-    if cp[0] != reales and cp[7] != reales and cp[6] != reales and cp[5] != reales:
+    if (cp[0] not in reales and cp[7] not in reales and cp[6] not in reales and cp[5] not in reales and cp[1] in reales
+            and cp[2] in reales and cp[3] in reales and cp[4] in reales):
         print("Argentina")
     else:
         print("Error")
