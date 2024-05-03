53df13fc9473a69895d95761bdaa6062c0b1a49a
Fragoz22 <frangomezfg22@gmail.com>
2024-05-02 21:55:03 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -16,8 +16,41 @@ if len(cp) > 9 or len(cp) < 4:
         print("Otros Países")
 
 else:
-    if (cp[0] not in reales and cp[7] not in reales and cp[6] not in reales and cp[5] not in reales and cp[1] in reales
+
+    if len(cp) == 4:
+        if cp[0] in reales and cp[1] in reales and cp[2] in reales and cp[3] in reales:
+            print("Bolivia")
+        else:
+            print("Otros países")
+
+    if len(cp) == 5:
+        if cp[0] in reales and cp[1] in reales and cp[2] in reales and cp[3] in reales and cp[4] in reales:
+            print("Uruguay")
+        else:
+            print("Otros países")
+
+    if len(cp) == 6:
+        if cp[0] in reales and cp[1] in reales and cp[2] in reales and cp[3] in reales and cp[4] in reales and cp[
+            5] in reales:
+            print("Paraguay")
+        else:
+            print("Otros países")
+
+    if len(cp) == 7:
+        if cp[0] in reales and cp[1] in reales and cp[2] in reales and cp[3] in reales and cp[4] in reales and cp[
+            5] in reales and cp[6] in reales:
+            print("Chile")
+        else:
+            print("Otros países")
+    if len(cp) == 8:
+        if (cp[0] not in reales and cp[7] not in reales and cp[6] not in reales and cp[5] not in reales and cp[1] in reales
             and cp[2] in reales and cp[3] in reales and cp[4] in reales):
-        print("Argentina")
-    else:
-        print("Error")
+            print("Argentina")
+        else:
+            print("Otros países")
+    if len(cp) == 9:
+        if cp[0] in reales and cp[1] in reales and cp[2] in reales and cp[3] in reales and cp[4] in reales and cp[
+            5] in reales and cp[6] in reales and cp[7] in reales and cp[8] in reales:
+            print("Brasil")
+        else:
+            print("Otros países")
