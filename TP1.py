489dee728b46a8cf01122168c0bcf46e00f58138
Fragoz22 <frangomezfg22@gmail.com>
2024-05-02 22:15:24 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -9,6 +9,10 @@ cp = input("Ingrese el código postal del lugar de destino: ")
 
 ban = True
 reales = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+provincia = ("Salta", "Provincia de Buenos Aires", "Ciudad Autónoma de Buenos Aires", "San Luis", "Entre Ríos",
+             "La Rioja", "Santiago del Estero", "Chaco", "San Juan", "Catamarca", "La Pampa", "Mendoza", "Misiones",
+             "Formosa", "Neuquén", "Río Negro", "Santa Fe", "Tucumán", "Chubut", "Tierra del Fuego", "Corrientes",
+             "Córdoba", "Jujuy", "Santa Cruz")
 
 if len(cp) > 9 or len(cp) < 4:
     ban = False
@@ -42,15 +46,18 @@ else:
             print("Chile")
         else:
             print("Otros países")
-    if len(cp) == 8:
-        if (cp[0] not in reales and cp[7] not in reales and cp[6] not in reales and cp[5] not in reales and cp[1] in reales
-            and cp[2] in reales and cp[3] in reales and cp[4] in reales):
-            print("Argentina")
-        else:
-            print("Otros países")
+
     if len(cp) == 9:
         if cp[0] in reales and cp[1] in reales and cp[2] in reales and cp[3] in reales and cp[4] in reales and cp[
             5] in reales and cp[6] in reales and cp[7] in reales and cp[8] in reales:
             print("Brasil")
         else:
             print("Otros países")
+    if len(cp) == 8:
+        if (cp[0] not in reales and cp[7] not in reales and cp[6] not in reales and cp[5] not in reales and cp[1] in reales
+            and cp[2] in reales and cp[3] in reales and cp[4] in reales):
+            print("Argentina")
+        else:
+            print("Otros países")
+
+
